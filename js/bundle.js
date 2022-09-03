207e1e7c7a14769c24dd7fb506ffb2a92e6c459d
Carlos Rodrigues <cerf@furg.br>
2022-09-03 12:07:39 -0300

    add another button to add new word

--- js/bundle.js
+++ js/bundle.js
@@ -30,6 +30,16 @@ function createWord() {
   sessionStorage.setItem('words', JSON.stringify(words))
   window.location.href = 'index.html'
 }
+function addNewWord() {
+  let words = sessionStorage.getItem('words')
+  if (!words) {
+    words = JSON.stringify([])
+  }
+  words = JSON.parse(words)
+  words.push(btoa(newWordInput.value.toUpperCase()))
+  newWordInput.value = ''
+  sessionStorage.setItem('words', JSON.stringify(words))
+}
 function startGame() {
   window.location.href = 'game.html'
 }
